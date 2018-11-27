9529693bb033b8803b27e5c84977271b0783e57c
Scott Raney <scott@matchism.org>
2018-11-27 15:25:52 -0700

    update

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,7 @@
 // magic incantations
 // tsc (to compile server)
 // ng build (to compile client)
-// ng build --aot (compile client with AOT)
+// ng build --aot (compile client with AOT option )
 // "\program files\mongodb\server\3.2\bin\mongod"
 // "\program files\mongodb\server\3.2\bin\mongorestore" --db matchdb --drop matchdb
 // set NODE_ENV=development& node server.js
